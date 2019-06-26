ddccce7ba39ed557714802fe2bcbfae1426ad120
Mike <lanselot777hailander@gmail.com>
2019-06-25 23:35:43 -0400

    Service worker

--- 02-service-worker/sw.js
+++ 02-service-worker/sw.js
@@ -1,7 +1,48 @@
-self.addEventListener('fetch', event => {    
+self.addEventListener('fetch', event => { 
+    /*   
+    if(event.request.url.includes('.jpg')){
+        console.log(event.request.url);
+        //let fotoReq = fetch('img/main.jpg');
+        //let fotoReq = fetch(event.request.url);
+        let fotoReq = fetch(event.request);
+        event.respondWith( fotoReq );
+    } 
+    */
+    /*
     if(event.request.url.includes('style.css')){
-        event.respondWith( null );
-    }else{
-        event.respondWith(fetch(event.request));
-    }    
+
+        let respuesta = new Response(`
+            body{
+                background-color: red !important;
+                color: pink;
+            }
+        `, {
+            headers:{
+                'Content-Type': 'text/css'
+            }
+        });
+        event.respondWith( respuesta );
+
+    } 
+    */
+   /*
+   if(event.request.url.includes('main.jpg')){
+       let resp = fetch('img/main-patas-arriba.jpg');
+        event.respondWith( resp );
+   }
+   */
+   //manejo de errores
+    const resp = fetch(event.request)
+        .then(resp => {
+            //console.log(resp);
+            /*
+            if (resp.ok) {
+                return resp;
+            } else {
+                return fetch('img/main.jpg');
+            }
+            */
+            return resp.ok ? resp : fetch('img/main.jpg');
+        });
+   event.respondWith(resp);
 });
